7f672d1cf4db6ec3db067d143a1b01cbefe612b4
105599004 <gay88358@yahoo.com.tw>
2021-11-19 15:23:15 +0800

    add cloud-provider-payment8001 module

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,5 @@
 public class Main {
+    // github project access token: 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
+    // git remote remove origin
+    // git remote add origin https://[token]@github.com/gay88358/spring-cloud-training.git
 }
