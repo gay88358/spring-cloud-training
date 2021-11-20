b0167bf16e543acb915fdf875eb2cfe453f2adc1
gay88358 <gay88358@yahoo.com.tw>
2021-11-20 10:17:00 +0800

    register cloud-provider-payment with eureka service

--- cloud-provider-payment8002/src/main/java/com/mars/hong/PaymentApplication8002.java
+++ cloud-provider-payment8002/src/main/java/com/mars/hong/PaymentApplication8002.java
@@ -2,8 +2,10 @@ package com.mars.hong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApplication8002 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication8002.class, args);
